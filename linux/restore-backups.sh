435deab8cb5c07bb4c78e17e7ae23bad607c1934
Tomé Carvalho <tomecarvalho01@gmail.com>
2025-12-21 15:23:44 +0000

    Clean up install and restore-backups scripts

--- linux/restore-backups.sh
+++ linux/restore-backups.sh
@@ -5,13 +5,11 @@
 
 set -e
 
-# Color codes for output
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
+NC='\033[0m' # No colour
 
-# Function to print colored messages
 print_info() {
   echo -e "${GREEN}[INFO]${NC} $1"
 }
